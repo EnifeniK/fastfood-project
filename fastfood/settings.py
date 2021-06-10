f88f3226154a8addae6784dbea545e8d719d4c8b
KEHINDE ENIFENI <enifenikehindeo@gmail.com>
2021-06-10 15:08:13 +0100

    new local settings

--- fastfood/settings.py
+++ fastfood/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+try:
+    from .local_settings import *
+except ImportError:
+    print('Looks like no local files. You must be on production!')
